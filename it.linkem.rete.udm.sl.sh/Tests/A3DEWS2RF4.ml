d9a282211b569d56ea56ee6399d8fefceff63ab4
stefano tofani <stefano.tofani@catenate.com>
2021-09-09 19:46:04 +0200

    implementata chiamata vs GetUser (SDB). create strutture per il parse del messaggio di output

---
+++ it.linkem.rete.udm.sl.sh/Tests/A3DEWS2RF4.ml
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="ASCII"?>
+<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
+  <CurrentLoadedTest href="it.linkem.rete.udm.sl.sh1.bwt#/"/>
+  <EmulationDataFile>ACED000574001D69742E6C696E6B656D2E726574652E75646D2E736C2E7368312E627774</EmulationDataFile>
+</emulation:BWTFile>
